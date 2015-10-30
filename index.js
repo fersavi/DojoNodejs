88d108d8d417edb89fdcfcd398c72d92b0734e88
fersavi <you@example.com>
2015-10-29 23:20:00 -0500

    Se quitó lineas de codigo innecesarias

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-var express = require('express');
-var app = express();
 
 
 app.set('port', (process.env.PORT || 8080));
